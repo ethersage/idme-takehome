c90528b8dde9ba0a9df76df6c300947f5462c73c
Joshua Kaminetsky <kami@Joshuas-MacBook-Pro.local>
2023-11-30 14:34:16 -0800

    fetch data and implement basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://storage.googleapis.com/marketplace-prod-7728-shop-cdn-e5e2/interview/data.json"
+    )
+      .then((response) => response.json())
+      .then((data) => setItems(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Purchases</h1>
+      <ul>
+        {items.map((item) => (
+          <li key={item.id}>
+            {/* Display one of the JSON fields for each item */}
+            <div>
+              <img src={item.location} />
+            </div>
+            <div>{item.name}</div>
+            <div>{item.purchaseDate}</div>
+            Price: {item.price}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
