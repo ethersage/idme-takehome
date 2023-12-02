8989a941dc5fbb6bdbec95bc7b626e5f7561f0b7
Joshua Kaminetsky <kami@Joshuas-MacBook-Pro.local>
2023-12-02 15:31:06 -0800

    category headings aligned

--- src/App.tsx
+++ src/App.tsx
@@ -20,16 +20,15 @@ function App() {
   return (
     <section>
       <h1>Purchases</h1>
-      <div className="purchase-heading-container">
-        <div className="purchase-heading">
-          <div className="purchase-item-heading">Name</div>
-          <div className="purchase-item-heading">Location</div>
-          <div className="purchase-item-heading">Purchase Date</div>
-          <div className="purchase-item-heading">Category</div>
-          <div className="purchase-item-heading">Description</div>
-          <div className="purchase-item-heading">Price</div>
-          {/* <div className="purchase-item-heading"></div> */}
-        </div>
+      <div className="purchase-list desktop">
+        <section className="purchase-item">
+          <div className="purchase-icon">Location</div>
+          <div className="purchase-name">Name</div>
+          <div className="purchase-description">Description</div>
+          <div className="purchase-date-container">Purchase Date</div>
+          <div className="purchase-price">Price</div>
+          <div className="purchase-category-container">Category</div>
+        </section>
       </div>
       <ul className="purchase-list">
         {purchaseData.map((data) => (
